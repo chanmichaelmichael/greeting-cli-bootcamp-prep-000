91200ebc40ac2e1bd851cbbd94bd619bee2f7a56
Michael Michael <michael.ta.chan@gmail.com>
2019-04-16 12:48:32 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,7 @@
 # code the #greeting method here!
 def greeting()
   puts  "Hi! I'm HAL, what's your name?"
-  name = gets.strip.chomp.strip
+  # name = gets.strip.chomp.strip
   name = name.strip.chomp
   puts "Hello #{name}. It's nice to meet you."
 end
